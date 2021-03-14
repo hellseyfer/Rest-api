646d22615606d0c27ed58fc948155194d97050d5
hellseyfer <hellseyfer@gmail.com>
2021-03-13 21:16:29 -0300

    twocheck post order post invoice

--- src/controllers/twocheck.controller.js
+++ src/controllers/twocheck.controller.js
@@ -7,5 +7,44 @@ twocheckCtrl.getNotifications = async (req, res) => {
     res.json(notifications);
 };
 
+twocheckCtrl.postInvoice = async (req, res) => {
+    console.log(req.body);
+    const newInvoice = new Object(req.body);
+
+    try {
+        await newInvoice.save();
+        console.log('promise all');
+            res.json({
+                'response': newInvoice
+            });
+            res.send('invoice received');
+
+    } catch (err) {
+        console.log(err);
+        var error = {};
+        error.message = err;
+        error.status = 27017;
+    }
+};
+
+twocheckCtrl.postOrder = async (req, res) => {
+    console.log(req.body);
+    const newOrder = new Object(req.body);
+
+    try {
+        await newOrder.save();
+        console.log('promise all');
+            res.json({
+                'response': newOrder
+            });
+            res.send('Order received');
+
+    } catch (err) {
+        console.log(err);
+        var error = {};
+        error.message = err;
+        error.status = 27017;
+    }
+};
 
 module.exports = twocheckCtrl;
--- src/routes/twocheck.routes.js
+++ src/routes/twocheck.routes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const twocheck = require('../controllers/twocheck.controller');
 
 router.get('/', twocheck.getNotifications); // user enters to / then...
+router.post('/invoice', twocheck.postInvoice);
+router.post('/order', twocheck.postOrder);
 
 module.exports = router;
